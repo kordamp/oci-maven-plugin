013d9a1da49c802a2610814687bc6a7428824d73
Andres Almiray <aalmiray@gmail.com>
2020-03-20 20:49:36 +0100

    Fix broken build

--- oci-maven-plugin/src/main/groovy/org/kordamp/maven/plugin/oci/mojos/traits/DnsLabelAwareTrait.groovy
+++ oci-maven-plugin/src/main/groovy/org/kordamp/maven/plugin/oci/mojos/traits/DnsLabelAwareTrait.groovy
@@ -36,13 +36,9 @@ trait DnsLabelAwareTrait implements PathAware, LogAware {
     String dnsLabel
 
     void setDnsLabel(String dnsLabel) {
-        this.@dnsLabel = normalizeDnsLabel(dnsLabel)
-    }
-
-    private String normalizeDnsLabel(String dnsLabel) {
         String label = dnsLabel?.replace('.', '')?.replace('-', '')
         if (label?.length() > 15) label = label?.substring(0, 14)
-        label
+        this.@dnsLabel = label
     }
 
     String getDnsLabel() {
